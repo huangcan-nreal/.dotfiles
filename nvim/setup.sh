e761346081232a9ca94ab093a27e1e9136ba8b26
huangcan <canhuang@nreal.ai>
2022-06-08 20:28:38 +0800

    update nvim

---
+++ nvim/setup.sh
@@ -0,0 +1,4 @@
+git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+nvim +PackerSync
+echo "Please nstall language server"
+echo "sudo npm install -g bash-language-server pyright vscode-langservers-extracted typescript typescript-language-server"
